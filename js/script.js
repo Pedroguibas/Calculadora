1f07eaecd16849ee1d2518deec7956177c82a58c
Pedro Guimarães Bastos <pedroguibas123@gmail.com>
2024-10-29 20:08:35 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,274 @@
+var display = document.getElementById('calcDisplay');
+var displayString = '0';
+var displayString2 = '';
+var multi = document.getElementById('multiply');
+var decimalAdded = false;
+var v1Added = false;
+var showedResult = false;
+var temaAzulApplied = true;
+var temaRoxoApplied = false;
+var temaEscuroApplied = true;
+var temaClaroApplied = false;
+var v1 = 0;
+var v2 = 0;
+var op = '';
+var displayOp = '';
+
+function addNumber(){
+    let num = this.value;
+    if(!showedResult)
+        if(!v1Added)
+        {
+            if(displayString == '0')
+                displayString = '';
+            displayString = displayString + num;
+        }
+        else   
+            displayString2 = displayString2 + num;    
+    else
+    {
+        displayString = '';
+        displayString = displayString + num;
+        showedResult = false;
+        v1Added = false;
+    }
+    display.textContent = displayString + displayOp + displayString2;
+}
+
+function addDecimal(){
+    if(!decimalAdded&&displayString != '')
+    {   
+        if(!v1Added)
+        {
+            displayString = displayString + '.';
+            showedResult = false;
+            display.textContent = displayString + displayOp + displayString2;
+        }
+        else
+        {
+            if(displayString2 != '')
+                displayString2 = displayString2 + '.';
+            else
+                displayString2 = '0.';
+        }
+        decimalAdded = true;
+
+        display.textContent = displayString + displayOp + displayString2;
+    }
+    else
+        if(displayString == '0'&&!v1Added)
+        {
+            displayString = displayString + '.';
+            display.textContent = displayString + displayOp + displayString2;
+        }
+}
+
+function addOp(){
+    if(displayString != '')
+    {
+        if(!v1Added)
+        {
+            v1Added = true;
+            showedResult = false;
+            v1 = parseFloat(displayString);
+            op = this.value;
+            displayOp = ' ' + op + ' ';
+            decimalAdded = false;      
+            display.textContent = displayString + displayOp + displayString2;
+        }
+        else
+        {
+            if(displayString2 != '')
+            {
+                if(op == '/'&&displayString2 == '0')
+                    window.alert('Não é possível dividr por zero.');
+                else
+                {
+                    v2 = parseFloat(displayString2);
+                    if(op == '+')
+                        result = v1 + v2;
+                       else
+                        if(op == '-')
+                            result = v1 - v2;
+                        else
+                            if(op == '*')
+                                result = v1 * v2;
+                            else
+                                if (op == '/')
+                                    result = v1 / v2;  
+                                else
+                                    if(op == '^') 
+                                        result = v1 ** v2;
+                                    else
+                                        result = v1 / 100 * v2;
+
+                    v1 = result;
+                    v2 = 0;
+                    displayString = result + '';
+                    displayString2 = '';
+                    op = this.value;
+                    displayOp = ' ' + op + ' ';
+                    display.textContent = displayString + displayOp + displayString2;
+                }   
+            }
+            else
+            {
+                op = this.value;
+                displayOp = ' ' + op + ' ';
+                display.textContent = displayString + displayOp + displayString2;
+            }
+            
+        }
+            decimalAdded = false;
+            showedResult = false;
+        
+    }
+}
+
+function showResult(){
+    if(op != ''&&displayString2 != '')
+    {
+        if(op == '/'&&displayString2 == 0)
+            window.alert('Não é possível dividir por zero.');
+        else
+        {
+            v2 = parseFloat(displayString2);
+            if(op == '+')
+                result = v1 + v2;
+            else    
+                if(op == '-')
+                    result = v1 - v2;
+                else
+                    if(op == '*')
+                        result = v1 * v2;
+                    else
+                        if(op == '/')
+                            result = v1 / v2;
+                        else
+                            if(op == '^')
+                                result = v1 ** v2;
+                            else
+                                result = v1 / 100 * v2;
+        
+            v2 = 0;
+            displayString2 = '';
+            displayString = result + '';
+            v1 = result;
+            displayOp = ' ' + op + ' ';
+            showedResult = true;
+            v1Added = false;
+
+            if(result % 1 == 0)
+                decimalAdded = false;
+            else
+                decimalAdded = true;
+
+            displayOp = '';
+        }
+    }
+    display.textContent = displayString + displayOp + displayString2;
+}
+
+
+function clear(){
+    v1 = 0;
+    v2 = 0;
+    op = '';
+    displayOp = '';
+    decimalAdded = false;
+    displayString = '0';
+    displayString2 = '';
+    v1Added = false; 
+    display.textContent = displayString + displayOp + displayString2;
+}
+
+function applyTemaAzul(){
+    if(!temaAzulApplied)
+    {
+        temaAzul.classList.add('selectedBtn');
+        temaRoxo.classList.remove('selectedBtn');
+
+        let elem = document.querySelector('.purpleSelected');
+        elem.classList.add('blueSelected');
+        elem.classList.remove('purpleSelected');
+
+        temaAzulApplied = true;
+        temaRoxoApplied = false;
+    }
+}
+
+function applyTemaRoxo(){
+    if(!temaRoxoApplied)
+    {   
+        temaRoxo.classList.add('selectedBtn');
+        temaAzul.classList.remove('selectedBtn');
+
+        let elem = document.querySelector('.blueSelected');
+        elem.classList.add('purpleSelected');
+        elem.classList.remove('blueSelected');
+
+        temaRoxoApplied = true;
+        temaAzulApplied = false;
+    }
+}
+
+function applyTemaClaro(){
+    if(!temaClaroApplied)
+    {
+        temaClaro.classList.add('selectedBtn');
+        temaEscuro.classList.remove('selectedBtn');
+
+        let elem = document.querySelector('.darkBody');
+        elem.classList.add('lightBody');
+        elem.classList.remove('darkBody');
+
+        temaClaroApplied = true;
+        temaEscuroApplied = false;
+    }
+}
+
+function applyTemaEscuro(){
+    if(!temaEscuroApplied)
+    {
+        temaEscuro.classList.add('selectedBtn');
+        temaClaro.classList.remove('selectedBtn');
+
+        let elem = document.querySelector('.lightBody');
+        elem.classList.add('darkBody');
+        elem.classList.remove('lightBody');
+
+        temaEscuroApplied = true;
+        temaClaroApplied = false;
+    }
+}
+
+var opBtn = document.querySelectorAll('.opBtn');
+opBtn.forEach(function(elem){
+    elem.addEventListener('click', addOp)
+});
+
+var numBtn = document.querySelectorAll('.numBtn');
+numBtn.forEach(function(elem){
+    elem.addEventListener('click', addNumber)
+});
+
+var clearBtn = document.getElementById('clear');
+clearBtn.addEventListener('click', clear);
+
+var resultBtn = document.getElementById('resultBtn');
+resultBtn.addEventListener('click', showResult);
+
+var decimalBtn = document.getElementById('decimal');
+decimalBtn.addEventListener('click', addDecimal);
+
+var temaAzul = document.getElementById('azulBtn');
+temaAzul.addEventListener('click', applyTemaAzul);
+
+var temaRoxo = document.getElementById('roxoBtn');
+temaRoxo.addEventListener('click', applyTemaRoxo);
+
+var temaClaro = document.getElementById('claroBtn');
+temaClaro.addEventListener('click', applyTemaClaro);
+
+var temaEscuro = document.getElementById('escuroBtn');
+temaEscuro.addEventListener('click', applyTemaEscuro);
