f9d09ca7d7d1e4dd8019a514d95a79f019820925
Pedro Guimarães Bastos <pedroguibas123@gmail.com>
2024-11-04 20:15:15 -0300

    Alterei link api. Tinha faltado alterar o link da API da segunda função.

--- js/script.js
+++ js/script.js
@@ -337,7 +337,7 @@ async function fetchExchangeValue(){
     let obj = await response.json();
     exchangeValue = obj.data[cur1];
     
-    response = await fetch(`https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_If4UH7rZwKkGQ5i66YcAVZTveiFaOFfqggvVEGEr&currencies=${cur2}`).catch((err) => console.error(err));
+    response = await fetch('https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_If4UH7rZwKkGQ5i66YcAVZTveiFaOFfqggvVEGEr&currencies=NZD%2CJPY%2CGBP%2CZAR%2CBRL%2CINR%2CIDR%2CKRW%2CCNY%2CSGD%2CEUR%2CCAD%2CNOK%2CCHF%2CSEK%2CAUD%2CHKD').catch((err) => console.error(err));
     obj = await response.json();
     exchangeValue2 = obj.data[cur2];
 
